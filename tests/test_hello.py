1c3900f6ef8d12ebf35fd9e41c1572504eafcad2
Dheraj77 <dherajkumar.vks0907@gmail.com>
2026-09-03 12:33:17 +0200

    Add test for hello API

---
+++ tests/test_hello.py
@@ -0,0 +1,7 @@
+from src.api import app
+
+def test_hello():
+    client = app.test_client()
+    response = client.get("/hello")
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Hello World!"}
