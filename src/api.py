e9b19a5c07a8f24a475cb03966401720f75737a9
yomal42 <yomalawarjana@gmail.com>
2026-09-07 14:31:34 +0200

    Fix Flask

--- src/api.py
+++ src/api.py
@@ -3,3 +3,6 @@ app = Flask(__name__)
 @app.route("/hello", methods=["GET"])
 def hello():
       return jsonify({"message":"Hello World!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
